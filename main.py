5d15dc77c817c717850a008656539d817ffcf56e
glitch54932 <hunterwillson895@gmail.com>
2026-09-15 10:49:54 +0300

    Added comments and centered the text for Title and Credits

--- main.py
+++ main.py
@@ -8,26 +8,28 @@ window = tk.Tk()
 
 def window_init():
 ###########################################################################################################
-    global window
+    global window                                          #Window
     window.title("Fisch Snail Macro")
     window.geometry("400x300")
-    window.configure(background="#051730")      #Window
+    window.configure(background="#051730")
     window.maxsize(400, 300)
-    window.minsize(400, 300)                       #Frame
+    window.minsize(400, 300)
 ###########################################################################################################
     btn_start = tk.Button(window, text="Start (F8)", bg="green", fg="white", font=("Comic Sans", 12, "bold"), height=1, width=6, command=lambda: start(keyboard.Key.f8))
     btn_start.place(x=65, y=200)                         #Start Button
 ###########################################################################################################
     btn_end = tk.Button(window, text="End (F7)", bg="red", fg="white", font=("Comic Sans", 12, "bold"), height=1, width=6, command=lambda: quit(keyboard.Key.f7))
-    btn_end.place(x=260, y=200)
+    btn_end.place(x=260, y=200)                          #End Button
 ###########################################################################################################
     ttl = tk.Text(window, height=1, width=16, bg="#051730", fg="white", font=("Comic Sans", 16, "bold"))
+    ttl.tag_configure("center", justify="center")        #Title
     ttl.place(x=110, y=25)
-    ttl.insert(tk.END, "Fisch Snail Macro")
+    ttl.insert(tk.END, "Fisch Snail Macro", "center")
 ###########################################################################################################
     credits = tk.Text(window, height=1, width=16, bg="#051730", fg="light grey", font=("Comic Sans", 9, "bold"))
+    credits.tag_configure("center", justify="center")   #Credits
     credits.place(x=150, y=70)
-    credits.insert(tk.END, "By @glitch54932")
+    credits.insert(tk.END, "By @glitch54932", "center")
 ###########################################################################################################
     window.mainloop()
 
@@ -61,8 +63,8 @@ def quit(key):
 
 if "__main__" == __name__:
 
-    listener = keyboard.Listener(on_press=start)
-    listener2 = keyboard.Listener(on_press=quit)
+    listener = keyboard.Listener(on_press=start)          #Starts key listener for F8 (start)
+    listener2 = keyboard.Listener(on_press=quit)          #Starts key listener for F7 (end)
     listener.start()
     listener2.start()
     print("[*] Fisch Snail Macro active.")
